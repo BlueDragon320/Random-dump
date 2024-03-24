e189cef9d8016fe8cb8a031dc52a24d0856825ba
Vaibhav <120330097+BlueDragon320@users.noreply.github.com>
2024-03-24 12:40:46 +0530

    test file

---
+++ helo.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main (){
+
+int age;
+cin>>age;
+if(age<12){
+    cout<<"Minecraft"<<endl;
+}
+else if(age<18){
+    cout<<"Reddit Gang"<<endl;
+}
+else if(age>18){
+    cout<<"Engineering Karle"<<endl;
+}
+else if(age>24){
+    cout<<"Kuch tho kaam karle"<<endl;
+}
+
+
+return 0;
+}
